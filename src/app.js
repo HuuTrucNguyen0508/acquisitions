d71beaeaf3ef4531877872e006e130780780f12d
Nguyen Huu Truc <nguyen.huutruc2001@gmail.com>
2025-09-18 03:32:56 +0200

    Adding auth

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import authRoutes from '#routes/auth.routes.js';
 const app = express();
 
 app.use(helmet());
@@ -18,5 +18,15 @@ app.get('/', (req, res) => {
     logger.info('Hello from aquisitions');
     res.status(200).send('Hello from aquisitions');
 });
+
+app.get('/health', (req, res) => {
+    res.status(200).send({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'Aquisitions API is running' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
